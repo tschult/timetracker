1fd612deef49b565a2cade0fa101ac097f9da401
Tino Schult <t.schult@euroimmun.de>
2020-05-11 08:35:16 +0200

    icons and theming

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
 import React, { Component } from 'react';
-import { Button, Grid, Typography, Box, Paper, Container} from '@material-ui/core';
+import { Button, Grid, Typography, Paper, Container, ThemeProvider, Switch } from '@material-ui/core';
 import Header from './Header';
-import BotNav from './BotNav';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { green, lightGreen } from '@material-ui/core/colors';
 
-export default class App extends Component { 
-  
-  state = {title: ''}
+export default class App extends Component {
 
-  render()
-  {
+  constructor(props) {
+    super(props);
+    this.state = {
+      darkMode: false
+    }
+  }
+
+  render() {
+    const theme = createMuiTheme({
+      palette: {
+        type: this.state.darkMode ? 'dark' : 'light',
+        primary: green,
+        secondary: lightGreen
+      }
+    });
     return (
-      <Grid container direction="column">
-        <Grid item>
-          <Header/>
-        </Grid>
-        <Grid item container>
-        <Container>
-          
-            <Typography variant="h5" component="h5" gutterBottom align="center">
-              Timetracker test
-            </Typography>
-            <Button variant="outlined" color="primary" fullWidth>Hello World</Button>
+      <ThemeProvider theme={theme}>
+        <Paper square style={{ height: "100vh" }}>
+          <Grid container direction="column">
+            <Grid item>
+              <Header />
+            </Grid>
+            <Grid item>
+              <Container>
 
-          </Container>
-          
-        </Grid>
-        <Grid item>
-          <BotNav/>
-        </Grid>
-      </Grid>
+                <Typography variant="h5" component="h5" gutterBottom align="center">
+                  Timetracker test
+                </Typography>
+                <Button variant="outlined" color="primary" fullWidth>Hello World</Button>
+                <Switch checked={this.state.darkMode} onChange={() => this.setState({ darkMode: !this.state.darkMode })} />
+
+              </Container>
+            </Grid>
+
+          </Grid>
+        </Paper>
+      </ThemeProvider>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import {ThemeProvider} from '@material-ui/core/styles';
-import theme from './theme';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <CssBaseline/>
-  <ThemeProvider theme={theme}>
-    
+    <CssBaseline/>    
     <App />
-  </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
